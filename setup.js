aea4da8579b26ab616d9ae606901f8ea5194e574
arloft <get@aaronarlof.me>
2016-12-18 12:03:10 -0500

    cleans, formats inventory file
    - removes two extra empty columns to right of quantity column
    - logs last row number for check purposes
    - removes two empty rows at bottom

--- setup.js
+++ setup.js
@@ -13,10 +13,6 @@ function setupInventorySheet() {
 }
 */
 
-/*	TODO
-	1. Remove the two rightmost columns added by GSheets
-*/
-
 function renameTheSheet() {
   var ss = SpreadsheetApp.getActiveSheet();
   var newSheetName = ss.getName();
@@ -27,13 +23,19 @@ function renameTheSheet() {
 // Revised version, deletes "asin" column, formula values are adjusted accordingly
 function setupInventorySheet() {
   var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("pq");
+  var lastRow = SpreadsheetApp.getActiveSheet().getLastRow();
+  Logger.log(lastRow); // logs lastRow, to verify no content rows are deleted
   ss.setFrozenRows(1);
   ss.setColumnWidth(1, 285);
   // Following 2 lines not needed with "asin" column deleted (line 32).
   // var asincolumn = ss.getRange("B:B");
   // asincolumn.setNumberFormat("0000000000");
   ss.sort(4, true);
+  ss.deleteColumn(6); // deletes 6th empty column
+  ss.deleteColumn(5); // deletes 5th empty column
   ss.deleteColumn(2); // deletes "asin" column
+  ss.deleteRow(lastRow+1); // delete last empty row
+  ss.deleteRow(lastRow+1); // delete last empty row
   ss.insertColumnAfter(2);
   var cellToReceivePriceFormula = ss.getRange("C2"); // C2 cell addresses updated
   cellToReceivePriceFormula.setValue("=round(sum(((B2*0.005)-B2)*-1),2)"); // B2 cell address updated
